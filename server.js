4aebf5c4b9a6b46ca1cacad13e2e6492fbe02ce0
D3bbie100 <113763116+D3bbie100@users.noreply.github.com>
2025-08-01 16:14:10 +0300

    server.js

--- server.js
+++ server.js
@@ -12,12 +12,15 @@ const getAccessToken = async () => {
     `${process.env.MPESA_CONSUMER_KEY}:${process.env.MPESA_CONSUMER_SECRET}`
   ).toString('base64');
 
-  // STK Push URL (production)
-const stkRes = await axios.post(
-  'https://api.safaricom.co.ke/mpesa/stkpush/v1/processrequest',
-  payload,
-  { headers: { Authorization: `Bearer ${accessToken}` } }
-);
+  const res = await axios.get(
+    'https://api.safaricom.co.ke/oauth/v1/generate?grant_type=client_credentials',
+    {
+      headers: {
+        Authorization: `Basic ${auth}`,
+      },
+    }
+  );
+
   return res.data.access_token;
 };
 
